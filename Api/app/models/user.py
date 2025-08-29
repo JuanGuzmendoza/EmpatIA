8fa741f0a922cc315be037cad5c2e5c16ef0885b
SebastianRdev <sebastianrdev@outlook.com>
2025-08-28 19:35:13 -0500

    models and schemas of user and genre

--- Api/app/models/user.py
+++ Api/app/models/user.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 from datetime import datetime
 
 Base = declarative_base()
@@ -7,13 +7,22 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id_user = Column(Integer, primary_key=True, index=True, autoincrement=True)
     full_name = Column(String(255), nullable=False)
-    username = Column(String(255), unique=True, nullable=False)
-    age = Column(Integer, nullable=False)
+    username = Column(String(255), unique=True, nullable=True)
     email = Column(String(255), unique=True, nullable=False)
-    phone = Column(Integer, nullable=True)
+    password_user = Column(String(255), nullable=False)
+    national_id = Column(String(100), unique=True, nullable=False)
+    age = Column(Integer, nullable=False)
+    id_genre = Column(Integer, ForeignKey("genres.id_genre"), nullable=False)
+    country = Column(String(100), nullable=False)
+    city = Column(String(100), nullable=False)
+    phone = Column(Integer, nullable=False)
+    emergency_contact = Column(Integer, nullable=True)
     address = Column(String(255), nullable=True)
-    user_profile = Column(String(255), nullable=True)
+    user_profile = Column(String(255), unique=True, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
-    update_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relación opcional con la tabla genres
+    genre = relationship("Genre", back_populates="users")
